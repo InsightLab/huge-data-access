bcb7b7200201544c658517d013948746d5b961ea
Lucas Vasconcelos <lucasvasco8@gmail.com>
2017-02-02 14:01:37 +0000

    Update manage segment function in BasedataAccess and test ByteArrayDataAccess

--- src/test/java/hugedataaccess/ByteArrayDataAccessTest.java
+++ src/test/java/hugedataaccess/ByteArrayDataAccessTest.java
@@ -1,7 +1,10 @@
 package hugedataaccess;
 
+import static org.junit.Assert.*;
+
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
 
 import hugedataaccess.ByteArrayDataAccess;
 import hugedataaccess.ByteBufferDataAccess;
@@ -16,6 +19,141 @@ public class ByteArrayDataAccessTest extends AbstractDataAccessTest {
 		dataAccess2.ensureCapacity(1024 * 1024 * 2);
 	}
 
+	@Test
+	public void testCaseManageSet1(){
+		
+		int currentPositionCorrect = 24;
+		int currentPositionResult;
+		
+		//Test if after insert int, the next insert go to next segment.
+		dataAccess1.setDouble(10);
+		dataAccess1.setInt(4);
+		dataAccess1.setLong(12);
+		
+		currentPositionResult = (int) dataAccess1.getCurrentPosition();
+		
+		assertEquals(currentPositionCorrect,currentPositionResult);
+
+	}
+	
+	@Test
+	public void testCaseManageSet2(){
+		
+		int currentPositionCorrect = 24;
+		int currentPositionResult;
+		
+		//Test if after insert float, the next insert go to next segment.
+		dataAccess1.setDouble(10);
+		dataAccess1.setFloat(4);
+		dataAccess1.setLong(12);
+		
+		currentPositionResult = (int) dataAccess1.getCurrentPosition();
+		
+		assertEquals(currentPositionCorrect,currentPositionResult);
+
+	}
+	
+	@Test
+	public void testCaseManageSet3(){
+		
+		int currentPositionCorrect = 24;
+		int currentPositionResult;
+		
+		//Test if after insert float, the next insert go to next segment.
+		dataAccess1.setDouble(10);
+		dataAccess1.setChar('c');
+		dataAccess1.setLong(12);
+		
+		currentPositionResult = (int) dataAccess1.getCurrentPosition();
+		
+		assertEquals(currentPositionCorrect,currentPositionResult);
+
+	}
+	
+	@Test
+	public void testCaseManageGet(){
+		
+		int currentPositionCorrect = 24;
+		int currentPositionResult;
+		double valueDouble;
+		int valueInt;
+		long valueLong;
+		
+		//Insert data
+		dataAccess1.setDouble(10);
+		dataAccess1.setInt(4);
+		dataAccess1.setLong(12);
+		
+		//Retrieve data
+		dataAccess1.setCurrentPosition(0);
+		valueDouble = dataAccess1.getDouble();
+		valueInt = dataAccess1.getInt();
+		valueLong = dataAccess1.getLong();
+		
+		currentPositionResult = (int) dataAccess1.getCurrentPosition();
+		
+		assertEquals(10, valueDouble, 0);
+		assertEquals(4, valueInt, 0);
+		assertEquals(12, valueLong, 0);
+		assertEquals(currentPositionCorrect,currentPositionResult);
+	}
+	
+	@Test
+	public void testCaseManageGet2(){
+		
+		int currentPositionCorrect = 24;
+		int currentPositionResult;
+		double valueDouble;
+		float valueFloat;
+		long valueLong;
+		
+		//Insert data
+		dataAccess1.setDouble(10);
+		dataAccess1.setFloat(4);
+		dataAccess1.setLong(12);
+		
+		//Retrieve data
+		dataAccess1.setCurrentPosition(0);
+		valueDouble = dataAccess1.getDouble();
+		valueFloat = dataAccess1.getFloat();
+		valueLong = dataAccess1.getLong();
+		
+		currentPositionResult = (int) dataAccess1.getCurrentPosition();
+		
+		assertEquals(10, valueDouble, 0);
+		assertEquals(4, valueFloat, 0);
+		assertEquals(12, valueLong, 0);
+		assertEquals(currentPositionCorrect,currentPositionResult);
+	}
+	
+	@Test
+	public void testCaseManageGet3(){
+		
+		int currentPositionCorrect = 24;
+		int currentPositionResult;
+		double valueDouble;
+		char valueChar;
+		long valueLong;
+		
+		//Insert data
+		dataAccess1.setDouble(10);
+		dataAccess1.setChar('c');
+		dataAccess1.setLong(12);
+		
+		//Retrieve data
+		dataAccess1.setCurrentPosition(0);
+		valueDouble = dataAccess1.getDouble();
+		valueChar = dataAccess1.getChar();
+		valueLong = dataAccess1.getLong();
+		
+		currentPositionResult = (int) dataAccess1.getCurrentPosition();
+		
+		assertEquals(10, valueDouble, 0);
+		assertEquals('c', valueChar, 0);
+		assertEquals(12, valueLong, 0);
+		assertEquals(currentPositionCorrect,currentPositionResult);
+	}
+	
 	@After
 	public void tearDown() {
 		dataAccess1.close();
